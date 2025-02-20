9eed3d35eb3a94a0040535e02033d33e0be88431
martijndan <martijn@firemail.cc>
2025-02-20 14:57:00 +0100

    terrible, ugly proof of concept hack :^)

--- terraform/main.tf
+++ terraform/main.tf
@@ -34,12 +34,13 @@ resource "docker_container" "cpp-build" {
     #the container's entry point, which is /usr/bin/cmake
     #entrypoint = ["ls"]
     mounts {
-        #target = local.docker_cpp_project_dir
+        target = local.docker_cpp_project_dir
         #FIXME: Lazy proof of concept hack:
         #We're running our Jenkins in a docker instance whilst using our host's docker daemon as our agent.
         #Docker does not like this and can't find the folder Jenkins reports as the folder 
-        target = "/home/martijn/jenkins/workspace/S8-Terraform"
-        source = local.parent_dir
+        #Ideally we use the value local.parent_dir, but we'll do that if we ever bother adding a different agent to run our jobs
+        source = "/home/martijn/jenkins/workspace/S8-Terraform"
+        #source = local.parent_dir
         type = "bind"
     }
     working_dir = local.docker_cpp_project_dir
