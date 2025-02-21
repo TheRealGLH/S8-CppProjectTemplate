a64f6c31afa830ef438fb1e871df1b0cdf628997
Martijn <TheRealGLH@users.noreply.github.com>
2025-02-21 17:17:42 +0100

    uh

---
+++ terraform/kubernetes.tf
@@ -0,0 +1,55 @@
+provider "kubernetes" {
+    host = "https://12.0.0.1:6443"
+    config_path = "~/.kube/config"
+    #config_context = "k3d-default"
+}
+resource "kubernetes_deployment" "nginx" {
+  metadata {
+    name = "scalable-nginx-example"
+    labels = {
+      App = "ScalableNginxExample"
+    }
+  }
+
+    depends_on = [
+        k3d_cluster.sample_cluster
+    ]
+
+  spec {
+    replicas = 2
+    selector {
+      match_labels = {
+        App = "ScalableNginxExample"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          App = "ScalableNginxExample"
+        }
+      }
+      spec {
+        container {
+          image = "nginx:1.7.8"
+          name  = "example"
+
+          port {
+            container_port = 80
+          }
+
+          resources {
+            limits = {
+              cpu    = "0.5"
+              memory = "512Mi"
+            }
+            requests = {
+              cpu    = "250m"
+              memory = "50Mi"
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
